fd38aeba58b8973ae65e8a8eaa4ac9b497ae194a
William Bonnaventure <william.bonnaventure@gmail.com>
2023-09-30 08:18:00 +0200

    Final fixes for PixelExperience build
    Change-Id: I1688f2363530d876b207413be27fb8d87b6504be

--- aosp_pioneer.mk
+++ aosp_pioneer.mk
@@ -21,12 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from pioneer device
 $(call inherit-product, device/sony/pioneer/device.mk)
 
-TARGET_USES_NQ_NFC := true
-USE_DEVICE_SPECIFIC_FM := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_FACE_UNLOCK_SUPPORTED := false
 
-# Inherit some common Lineage stuff.
-#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
